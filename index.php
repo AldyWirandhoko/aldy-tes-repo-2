b32a65ac4bdd96cd2c7a20f291a15dafe5a9aecd
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-02-19 21:51:24 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h2>Hello World</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus repudiandae nemo dolorum obcaecati ullam veritatis doloremque iusto ipsam deleniti voluptatibus!</p>
 </body>
 </html>
